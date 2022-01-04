5c84aab8c822d7ec42fcab83b2e862e228f1b383
MICHAEL <MJENNINGS6@gmail.com>
2022-01-04 12:22:19 -0500

    completed
    first attempt I had to some reserch bc my initial approach as with the loop was wonky and I couldnt quite wtap my head around it.

---
+++ veryBigSum.js
@@ -0,0 +1,40 @@
+// In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+// Function Description
+
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+// aVeryBigSum has the following parameter(s):
+
+// int ar[n]: an array of integers .
+// Return
+
+// long: the sum of all array elements
+// Input Format
+
+// The first line of the input consists of an integer n.
+// The next line contains n space-separated integers contained in the array.
+
+// Sample Input
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+
+// Output
+// 5000000015
+
+function aVeryBigSum(ar) {
+  // Write your code here
+  // const reducer = (preVal, curVal) => preVal + curVal
+  // let res = ar.reduce(reducer)
+  // return res
+
+  // create a variable to hold the sum or result
+  let res = 0;
+  // loop throug the array
+  for (let i = 0; i < ar.length; i++) {
+    // add the value at each index to the res variable
+    res += ar[i];
+  }
+  // return the result
+  return res;
+}
